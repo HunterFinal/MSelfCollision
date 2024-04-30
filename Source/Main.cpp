a5dc6138c0c8dc3dfdc619462d1592e8efb566c0
HunterFinal <schiffenficken@gmail.com>
2024-04-30 12:31:30 +0900

    add c# like Property

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -7,6 +7,28 @@ using namespace Core;
 
 int main()
 {
+	Transform2D* xx;
+
+	xx = new Transform2D;
+
+	xx->Test = 1;
+
+	int x;
+
+	x = xx->Test;
+
+	std::cout << x << std::endl;
+
+	x = 100;
+
+	std::cout << xx->Test << std::endl;
+
+	std::cout << x << std::endl;
+
+	std::cout << xx->ReadonlyTest << std::endl;
+
+	delete xx;
+	xx = nullptr;
 
 	return 0;
 }
