ee6be8d5db1273ad3882a113c6cbfb942e2e4d46
バクチソウ <schiffenficken@gmail.com>
2024-09-24 00:25:52 +0900

    update？

--- Source/Utilities/MDesignPattern/singleton.h
+++ Source/Utilities/MDesignPattern/singleton.h
@@ -8,54 +8,158 @@
 //                          {
 //                              friend class Singleton<SingletonClass>;
 //                              private:
-//                                  SingletonClass() = default;             // Constructor need to be private or protected
-//                              public:
-//                                  ~SingletonClass() = default;            // Destructor need to be public
+//                                  SingletonClass() = default;             // Constructor and Destructor need to be private or protected
+//                                  ~SingletonClass() = default;            
 //                          }
 // Update:              2024/06/17  Create
+//                      2024/08/15  Change GetInstance to raw-pointer(it was shared_ptr before)
+//                                  Add Singleton GC(prevent memory leak if someone forget to release manually)
 //
 // Author:              MAI ZHICONG
 // ---------------------------------------------------------------------------------------------------------------------------------------
 
 #pragma once
 
-#include <memory>   // std::shared_ptr
+//#include <memory>   // std::shared_ptr
+
+#include "thread_safe_def.h"
+#include <cassert>
+
+#include <atomic>
 
 namespace MDesignPattern
 {
     namespace MSingleton
     {
-        // ã‚·ãƒ³ã‚°ãƒ«ãƒˆãƒ³
+        // ƒVƒ“ƒOƒ‹ƒgƒ“
         template<typename T>
         class Singleton
         {
+        private:
+            
+            // GC class
+            class SingletonGC
+            {
+                public:
+                    ~SingletonGC()
+                    {
+                        T* temp = _instance.load();
+                        if (temp != nullptr)
+                        {
+                            // TODO need test
+                            delete temp;
+                        }
+                    }
+            };
 
         public:
-            static inline std::shared_ptr<T> GetInstance()
-            {
-                // ã‚¤ãƒ³ã‚¹ã‚¿ãƒ³ã‚¹ç”Ÿæˆã¯ä¸€å›ã—ã‹å®Ÿè¡Œã—ãªã„
-                static std::once_flag flag;
-                std::call_once(flag,[&]()
-                {
-                    _instance = std::shared_ptr<T>(new T);
-                });
+            //
+            // smart pointer version
+            //
+            // static inline std::shared_ptr<T> GetInstance()
+            // {
+            //     // ƒCƒ“ƒXƒ^ƒ“ƒX¶¬‚Íˆê‰ñ‚µ‚©Às‚µ‚È‚¢
+            //     static std::once_flag flag;
+            //     std::call_once(flag,[&]()
+            //     {
+            //         _instance = std::shared_ptr<T>(new T);
+            //     });
 
-                return _instance;
+            //     return std::make_shared<T>(_instance);
+            // }
+
+            /// <summary>
+            /// ƒVƒ“ƒOƒ‹ƒgƒ“‚ğ‰Šú‰»‚·‚é
+            /// </summary>
+            /// <typeparam name="...Args">‰Šú‰»q</typeparam>
+            /// <param name="...args"></param>
+            template<typename... Args>
+            static inline void CreateInstance(Args... args)
+            {
+                // TODO need understand atomic
+                T* instancePtr = _instance.load(std::memory_order_relaxed);
+                std::atomic_thread_fence(std::memory_order_acquire);
+
+                if (instancePtr == nullptr)
+                {
+                    LOCK(_padlock)
+                    instancePtr = _instance.load(std::memory_order_relaxed);
+
+                    if (instancePtr == nullptr)
+                    {
+                        instancePtr = new T(args...);
+                        std::atomic_thread_fence(std::memory_order_release);
+                        _instance.store(instancePtr,std::memory_order_relaxed);
+
+                        // atexit can register at least 32 functions
+                        // but......
+                        // limit is implementation-defined
+                        // so I can't use it in a template class
+
+                        // atexit(ReleaseInstance);
+                        
+                    } // end of second null check
+                } // end of first null check
             }
 
+            static inline T* GetInstance()
+            {
+                return _instance.load();
+            }
+
+            static inline void ReleaseInstance()
+            {
+                T* instancePtr = _instance.load(std::memory_order_relaxed);
+                std::atomic_thread_fence(std::memory_order_acquire);
+                if (instancePtr != nullptr)
+                {
+                    LOCK(_padlock);
+                    instancePtr = _instance.load(std::memory_order_relaxed);
+
+                    if (instancePtr != nullptr)
+                    {
+                        delete instancePtr;
+                        instancePtr = nullptr;
+
+                        std::atomic_thread_fence(std::memory_order_release);
+                        _instance.store(instancePtr,std::memory_order_relaxed);
+                    } // end of second null check
+                } // end of first null check
+            }
+
+
         protected:
-            Singleton() {}
+            Singleton() = default;
+            virtual ~Singleton() = default;
 
         private:
-            Singleton(const Singleton& other) = delete;
-            Singleton& operator=(const Singleton& other) = delete;
+            Singleton(const Singleton&) = delete;
+            Singleton& operator=(const Singleton&) = delete;
+
+        // private:
+        //    void operator delete(void* ptr) noexcept
+        //    {
+        //        ((T*)ptr)->~T();
+        //        free(ptr);
+        //    }
 
         private:
-            static std::shared_ptr<T> _instance;
+            static std::atomic<T*> _instance;
+            static std::mutex _padlock;
+            // must place this under _instance
+            static SingletonGC _gcObj;
         };
 
+        // static member initialize
         template<typename T>
-        std::shared_ptr<T> Singleton<T>::_instance = nullptr;
+        std::atomic<T*> Singleton<T>::_instance;
+        template<typename T>
+        std::mutex Singleton<T>::_padlock;
+        // TODO
+        // need typename in front of it(tell compiler it is a type)
+        template<typename T>
+        typename Singleton<T>::SingletonGC Singleton<T>::_gcObj;
+
     }// namespace MSingleton
 
 }// namespace MDesignPattern
--- Source/Utilities/thread_safe_def.h
+++ Source/Utilities/thread_safe_def.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#ifndef THREAD_SAVE || _THREAD_SAVE
 #define THREAD_SAVE
 
 #include <mutex>
 
 #define LOCK(padlock) { std::lock_guard<std::mutex>guard(padlock);}
+#endif
